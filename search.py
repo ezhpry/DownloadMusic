b82c8117cec9a67d59707bea23f53b4fd0c1c299
zzz <zephyr0063@icloud.com>
2025-09-17 17:41:12 +0800

    完成简单的下载功能，根据关键字下载

--- search.py
+++ search.py
@@ -2,6 +2,12 @@ import json
 
 import requests
 
+def printa(data):
+    print("数据：", data)
+    print("类型：", type(data))
+
+
+
 class SearchEngine:
     """
     Download songs from QQMusic
@@ -10,27 +16,49 @@ class SearchEngine:
         self.data=json.loads(open('search_api.json', 'r', encoding='utf-8').read())
         self.url=self.data['url']
 
-    def search_by_input_word(self):
-        keyword=input("请输入搜索关键词：")
+    def search_by_word(self,keyword)->dict:
         url = self.url + '?word=' + keyword
         response = requests.get(url)
         return response.json()
 
+    def get_first_songId(self,search_result:dict)->str:
+        return search_result['data'][0]['id']
 
-    def search_by_word(self,keyword):
-        url = self.url + '?word=' + keyword
-        response = requests.get(url)
-        return response.json()
-
-    def search_by_id(self, id):
+    def search_song_byId(self,id):
         url = self.url + '?id=' + str(id)
         response = requests.get(url)
-        return response.json()
+        resp=response.json()
+        return {
+            'song':resp['data']['song'],
+            'singer':resp['data']['singer'],
+            'url':resp['data']['url']
+        }
 
-    def get_song_firstId(self,search_result)->str:
-        firstId=search_result['data']['id']
-        return firstId
+
+    def search(self,data,mode='k',):
+        """
+        核心搜索功能
+        :param data:
+        :param mode: 搜索模式，默认为‘k'，即搜索关键字（歌曲名字，歌手名字）
+        :return:
+        """
+        if mode=='k':
+            searchResult=self.search_by_word(data)
+            songId=self.get_first_songId(searchResult)
+            songInfo=self.search_song_byId(songId)
+            return songInfo
+
+    def download(self,songInfo:dict,save_path:str='./'):
+        songName=songInfo['song']
+        songSinger=songInfo['singer']
+        songUrl=songInfo['url']
+        response = requests.get(songUrl)
+        with open(save_path+'/'+songName+'-'+songSinger+'.flac', 'wb') as f:
+            f.write(response.content)
+            print(f'[*]下载成功: {songName} - {songSinger}.flac')
 
 sh=SearchEngine()
 
-info=sh.search_by_word('lemon tree')
+songInfo=sh.search('半岛铁盒')
+sh.download(songInfo,save_path='./')
+
